e64910d2523ae1528c196b0bc413772edc29bac7
Chris.Openshaw <copenshaw17@gmail.com>
2023-09-08 09:17:12 -0500

    added form

--- Develop/script.js
+++ Develop/script.js
@@ -2,26 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  let valid = false;
-  while (!valid) {
-    let passwordLength = prompt("Please enter your desired password length (8-128)");
-    var passwordInt = parseInt(passwordLength)
-    console.log(passwordInt)
-
-    if (passwordInt >= 8 && passwordInt <= 128) {
-      alert("Choose at least one special character to include in your password!")
-
-      document.getElementById("character-prompt").style.display = "flex"; 
-      document.getElementById("character-prompt").style.direction = "row";
-      valid = true;
-    } else {
-      alert("Please choose a number between 8 and 128!")
-    }
-  }
 }
 
 // Write password to the #password input
 function writePassword() {
+  document.getElementById("character-prompt").style.display = "none"; 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
